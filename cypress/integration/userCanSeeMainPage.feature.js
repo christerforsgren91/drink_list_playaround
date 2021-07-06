990831027cf6e9216930bdcde0f62590b42326f7
Christer <christerforsgren91@gmail.com>
2021-07-06 11:22:34 +0200

    Add test for main page Add fixture for drinks

---
+++ cypress/integration/userCanSeeMainPage.feature.js
@@ -0,0 +1,18 @@
+describe('User can see main page layout', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'www.thecocktaildb.com/api/json/v1/1/search.php?s', {
+      fixture: 'drinks.json',
+    })
+    cy.visit('/')
+  })
+
+  it('is expected to show header', () => {
+    cy.get('[data-cy=logo]').should('exist')
+    cy.get('[data-cy=site-name]').should('contain', 'Shake And Make')
+  })
+
+  it('is expected to show list of drinks', () => {
+    cy.get('[data-cy=drink-card]').should('have.length', 25)
+    cy.get('[data-cy=name]').should('contain', 'GG')
+  })
+})
