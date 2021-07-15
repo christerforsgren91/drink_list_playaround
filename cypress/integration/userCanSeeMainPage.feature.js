98a30fa386edc980c5f2e4fb457daf339b8e0960
Christer <christerforsgren91@gmail.com>
2021-07-15 13:44:20 +0200

    Add test for drink information when click on a card

--- cypress/integration/userCanSeeMainPage.feature.js
+++ cypress/integration/userCanSeeMainPage.feature.js
@@ -14,4 +14,16 @@ describe('User can see main page layout', () => {
     cy.get('[data-cy=drink-card]').should('have.length', 25)
     cy.get('[data-cy=name]').should('contain', 'GG')
   })
+
+  it('is expected to show drink information', () => {
+    cy.get('[data-cy=drink-card]').onClick().within(() => {
+      cy.get('[data-cy=drink-title]').should('contain', 'GG')
+      cy.get('[data-cy=drink-img]').should('exist')
+      cy.get('[data-cy=glass]').should('contain', 'Collins Glass')
+      cy.get('[data-cy=drink-instruction]').should('contain', 'Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.')
+      cy.get('[data-cy=drink-ingridient]').should('have.length', 15)
+      cy.get('[data-cy=drink-measure]').should('have.length', 15)
+      cy.get('[data-cy=close-button]').should('should', 'exist')
+    })
+  })
 })
